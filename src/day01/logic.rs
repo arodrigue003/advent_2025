8d72405d5ed5816fc39273ce024c63aec1982bba
arodrigue003 <adrien.rod@aliceadsl.fr>
2025-12-01 22:56:59 +0100

    Refactor: Optimize day 01 solution

--- src/day01/logic.rs
+++ src/day01/logic.rs
@@ -1,30 +1,28 @@
 pub fn solve_part_one(data: &[(char, i64)]) -> u64 {
-    data.iter().fold((50,0), |(mut pos, mut count), (direction, amount)| {
-        pos = match direction {
-            'L' => (pos - amount).rem_euclid(100),
-            'R' => (pos + amount).rem_euclid(100),
-            _ => unreachable!()
-        };
-        if pos == 0 {
-            count += 1;
-        };
-        (pos, count)
-    }).1
+    data.iter()
+        .fold((50, 0), |(pos, count), (direction, amount)| match direction {
+            'L' => (
+                (pos - amount).rem_euclid(100),
+                count + ((pos - amount) % 100 == 0) as u64,
+            ),
+            'R' => (
+                (pos + amount).rem_euclid(100),
+                count + ((pos + amount) % 100 == 0) as u64,
+            ),
+            _ => unreachable!(),
+        })
+        .1
 }
 
 pub fn solve_part_two(data: &[(char, i64)]) -> i64 {
-    data.iter().fold((50,0), |(mut pos, mut count), (direction, amount)| {
-         match direction {
-            'L' => {
-                count += ((100 - pos)%100 + amount) / 100;
-                pos = (pos - amount).rem_euclid(100);
-            },
-            'R' => {
-                count += (pos + amount) / 100;
-                pos = (pos + amount).rem_euclid(100);
-            },
-            _ => unreachable!()
-        };
-        (pos, count)
-    }).1
+    data.iter()
+        .fold((50, 0), |(pos, count), (direction, amount)| match direction {
+            'L' => (
+                (pos - amount).rem_euclid(100),
+                count + ((100 - pos) % 100 + amount) / 100,
+            ),
+            'R' => ((pos + amount).rem_euclid(100), count + (pos + amount) / 100),
+            _ => unreachable!(),
+        })
+        .1
 }
