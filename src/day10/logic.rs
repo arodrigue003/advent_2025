8dbee4be054f5d88d62f238580bfc6aa5d139442
arodrigue003 <adrien.rod@aliceadsl.fr>
2025-12-15 21:31:20 +0100

    Add: Add a naive solution to day 10 part 2

--- src/day10/logic.rs
+++ src/day10/logic.rs
@@ -1,6 +1,10 @@
 use crate::day10::models::Machine;
-use fraction::{Fraction, ToPrimitive};
-use ndarray::{concatenate, Array2, Axis, Zip};
+use fraction::{Fraction, ToPrimitive, Zero};
+use itertools::Itertools;
+use ndarray::linalg::Dot;
+use ndarray::{concatenate, stack, Array2, Axis, Ix, Zip};
+use std::fmt::{write, Display, Formatter};
+use std::ops::Range;
 
 pub fn solve_part_one(machines: &[Machine]) -> u32 {
     let mut res = 0;
@@ -57,7 +61,7 @@ fn gauss_jordan(equations: &Array2<Fraction>, targets: &Array2<Fraction>) -> Arr
     // Create the matrix
     let mut matrix: Array2<Fraction> = concatenate![Axis(1), equations.clone(), targets.clone()];
 
-    println!("{}", &matrix);
+    // println!("{}", &matrix);
 
     // Get the sizes
     let (m, n) = matrix.dim();
@@ -110,30 +114,82 @@ fn gauss_jordan(equations: &Array2<Fraction>, targets: &Array2<Fraction>) -> Arr
     matrix
 }
 
-fn get_particular_solution(equations: &Array2<Fraction>, targets: &Array2<Fraction>) -> Array2<i64> {
+struct FreeVariable {
+    idx: Ix,
+    min: Option<i64>,
+    max: Option<i64>,
+}
+
+impl FreeVariable {
+    pub fn new(idx: Ix) -> Self {
+        Self {
+            idx,
+            min: Some(0),
+            max: None,
+        }
+    }
+
+    pub fn new_max(&mut self, new_max: i64) {
+        if new_max < 0 {
+            unreachable!()
+        }
+        if let Some(max) = self.max {
+            self.max = Some(max.min(new_max));
+        } else {
+            self.max = Some(new_max);
+        }
+    }
+
+    pub fn new_min(&mut self, new_min: i64) {
+        if new_min < 0 {
+            unreachable!()
+        }
+        if let Some(min) = self.min {
+            self.min = Some(min.max(new_min));
+        } else {
+            self.min = Some(new_min);
+        }
+    }
+
+    pub fn get_range(&self) -> Range<i64> {
+        self.min.unwrap()..self.max.unwrap()
+    }
+}
+
+impl Display for FreeVariable {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: (", self.idx)?;
+
+        if let Some(min) = self.min.as_ref() {
+            write!(f, "min={},", min)?;
+        }
+
+        if let Some(max) = self.max.as_ref() {
+            write!(f, "max={},", max)?;
+        }
+
+        write!(f, ")")
+    }
+}
+
+fn get_min_button_press(equations: &Array2<Fraction>, targets: &Array2<Fraction>) -> i64 {
     // Solve the system and find a particular solution
     let solutions = gauss_jordan(equations, targets);
 
-    println!("{}", &solutions);
+    // println!("{}", &solutions);
 
-    let mut result = Array2::zeros((equations.dim().1, 1));
     let mut free_variables = vec![];
 
     // Get the sizes
     let (m, n) = solutions.dim();
+    // println!("Dimensions: {}x{}", m, n);
 
     let mut h = 0; // pivot line
     let mut k = 0; // Pivot column
 
-    while h < m && k < n {
-        if solutions[[h,k]] != Fraction::from(0) {
+    while h < m && k < n - 1 {
+        if solutions[[h, k]] != Fraction::from(0) {
             // We found a value for the k button
-            let solution = solutions[[h, n-1]];
-            if *solution.denom().unwrap() != 1 {
-                // Solution must be integer
-                unreachable!()
-            }
-            result[[k,0]] = solution.to_i64().unwrap();
 
             // Go to the next line and the next column
             h += 1;
@@ -147,16 +203,102 @@ fn get_particular_solution(equations: &Array2<Fraction>, targets: &Array2<Fracti
         }
     }
 
-    println!("{h}/{m}-{k}/{n}");
-
     // Add remaining columns
-    for remaining in k..(n-1) {
+    for remaining in k..n - 1 {
         free_variables.push(remaining);
     }
 
-    println!("{:?}", &free_variables);
+    // println!("Free variables ({}): {:?}", free_variables.len(), &free_variables);
 
-    result
+    // Build the equation we want to minimize
+    //  * The first value is 0, the others are 1 to take into account the expression of the free
+    //    variable
+    let mut eq_to_minimize = vec![Fraction::from(1); free_variables.len() + 1];
+    eq_to_minimize[0] = Fraction::zero();
+    //  * Add the expression of others variables
+    for h in 0..m {
+        eq_to_minimize[0] += solutions[[h, n - 1]];
+        for (idx, free_variable) in free_variables.iter().enumerate() {
+            eq_to_minimize[idx + 1] -= solutions[[h, *free_variable]];
+        }
+    }
+
+    // println!("{:?}", &eq_to_minimize);
+
+    // Add constraint to each free variable
+    let mut free_variables_2: Vec<FreeVariable> = free_variables.iter().map(|idx| FreeVariable::new(*idx)).collect();
+
+    // Add constraint from the initial array
+    for h in 0..m {
+        for (pos, free_variable) in free_variables.iter().enumerate() {
+            if equations[[h, *free_variable]] != Fraction::zero() {
+                free_variables_2[pos].new_max(targets[[h, 0]].to_i64().unwrap());
+            }
+        }
+    }
+
+    // for free_variable in &free_variables_2 {
+    //     println!("{}", free_variable);
+    // }
+
+    // Create the generic solution matrix
+    let lines = n - 1 - free_variables.len();
+    //  * Concatenate the solution column and the free variable ones
+    let views: Vec<_> = std::iter::once(n - 1)
+        .chain(free_variables)
+        .map(|col| solutions.column(col))
+        .collect();
+    let mut generic_solution = stack(Axis(1), &views).unwrap();
+    //  * Invert columns 1..n to express xi values
+    generic_solution.slice_mut(ndarray::s![.., 1..]).mapv_inplace(|x| -x);
+
+    // println!("{}", &generic_solution);
+
+    let mut min_combo = i64::MAX;
+    for combo in std::iter::once(1..2)
+        .chain(free_variables_2.iter().map(|var| var.get_range()))
+        .multi_cartesian_product()
+    {
+        // println!("combo => {:?}", &combo);
+
+        // Check if we have a better value
+        let eq_to_minimize_value: Fraction = eq_to_minimize.iter().zip(combo.iter()).map(|(a, b)| a * *b).sum();
+
+        if eq_to_minimize_value.denom().unwrap() != &1 {
+            // We need an integer result
+            continue;
+        }
+
+        let eq_to_minimize_value = eq_to_minimize_value.to_i64().unwrap();
+
+        // println!("{eq_to_minimize_value}");
+
+        if eq_to_minimize_value < min_combo {
+            // Check that every value is positive
+            //  * Create a solution vector from this value
+            let row = Array2::from_shape_vec((combo.len(), 1), combo.into_iter().map(|c| Fraction::from(c)).collect())
+                .unwrap();
+
+            // println!("{}", row);
+
+            //  * Compute the product
+            let result = generic_solution.dot(&row);
+
+            //  * If any value is negative, our solution is not valid
+            if result.iter().any(|v|v.is_sign_negative()) {
+                continue
+            }
+
+            //  * Update the min_combo value
+            min_combo = eq_to_minimize_value;
+        }
+
+        // Create the free range solution vec
+    }
+
+    // println!("{min_combo}");
+
+    min_combo
 }
 
 pub fn solve_part_two(machines: &[Machine]) -> i64 {
@@ -187,34 +329,39 @@ pub fn solve_part_two(machines: &[Machine]) -> i64 {
     //
     // gauss_jordan(&equations, &solutions);
 
-    // Create test values
-    #[rustfmt::skip]
-    let equations = Array2::from_shape_vec(
-        (3, 4),
-        vec![
-            1, 2, 1, 1,
-            2, 4, 2, 3,
-            3, 6, 2, 5
-        ]
-            .into_iter()
-            .map(|v| Fraction::from(v))
-            .collect(),
-    ).unwrap();
+    // // Create test values
+    // #[rustfmt::skip]
+    // let equations = Array2::from_shape_vec(
+    //     (3, 4),
+    //     vec![
+    //         1, 2, 1, 1,
+    //         2, 4, 2, 3,
+    //         3, 6, 2, 5
+    //     ]
+    //         .into_iter()
+    //         .map(|v| Fraction::from(v))
+    //         .collect(),
+    // ).unwrap();
+    //
+    // #[rustfmt::skip]
+    // let solutions = Array2::from_shape_vec(
+    //     (3, 1),
+    //     vec![
+    //         4,
+    //         7,
+    //         10
+    //     ].into_iter().map(|v| Fraction::from(v)).collect(),
+    // )
+    // .unwrap();
+    //
+    // println!("{}", gauss_jordan(&equations, &solutions));
 
-    #[rustfmt::skip]
-    let solutions = Array2::from_shape_vec(
-        (3, 1),
-        vec![
-            4,
-            7,
-            10
-        ].into_iter().map(|v| Fraction::from(v)).collect(),
-    )
-    .unwrap();
-
-    println!("{}", gauss_jordan(&equations, &solutions));
+    let mut result = 0;
 
     for (i, machine) in machines.iter().enumerate() {
+        // if i != 144 {
+        //     continue
+        // }
         println!("======Machine {i}======");
 
         // Create the base system of equation with ndarray
@@ -236,13 +383,8 @@ pub fn solve_part_two(machines: &[Machine]) -> i64 {
 
         // Find a specific solution
         // gauss_jordan(&equations, &solutions);
-        println!("{}", get_particular_solution(&equations, &solutions));
-
-        // Reduce the equations to identify free variables
-        println!("{}", gauss_jordan(&equations, &Array2::zeros((machine.lights.len(), 1))));
-
-
-
+        result += get_min_button_press(&equations, &solutions);
     }
-    0
+
+    result
 }
